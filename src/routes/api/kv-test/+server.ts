b6f37206b22eb2442ada30547667f8f93bb872c9
Balint Hegyi <bhegyi@gradle.com>
2023-01-20 00:20:16 +0000

    Bump KV

--- src/routes/api/kv-test/+server.ts
+++ src/routes/api/kv-test/+server.ts
@@ -2,8 +2,8 @@ import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { v4 as uuidv4 } from 'uuid';
 
-export const GET: RequestHandler = async ({ platform }) => {
-	const ES_TEST_NAMESPACE = platform?.env?.ES_TEST_NAMESPACE;
+export const GET: RequestHandler = async (event) => {
+	const ES_TEST_NAMESPACE = event.platform?.env?.ES_TEST_NAMESPACE;
 
 	await platform?.env?.ES_TEST_NAMESPACE.put(String(uuidv4()), JSON.stringify(new Date()), {
 		metadata: { someMetadataKey: 'someMetadataValue' }
