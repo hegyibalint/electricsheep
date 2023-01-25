0f5cc4b8f9687c5823cf0a7e7ee57cd844dbc844
Balint Hegyi <bhegyi@gradle.com>
2023-01-25 20:47:44 +0000

    Move out API server parts

--- svelte.config.js
+++ svelte.config.js
@@ -5,10 +5,7 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess({
-		script: "typescript",
-		style: "postcss"
-	}),
+	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter()
